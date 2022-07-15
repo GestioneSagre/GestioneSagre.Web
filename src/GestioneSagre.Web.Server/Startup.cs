240ff7975bd0b169f8d8634ff0a07c3649b8e07c
Angelo Pirola <angelo9871@gmail.com>
2022-07-15 20:50:39 +0200

    Aggiornato progetto GestioneSagre.Web.Server

--- src/GestioneSagre.Web.Server/Startup.cs
+++ src/GestioneSagre.Web.Server/Startup.cs
@@ -1,52 +1,80 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using GestioneSagre.Addons.Extensions;
+using GestioneSagre.Business.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
 
-namespace GestioneSagre.Web.Server
+namespace GestioneSagre.Web.Server;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+        services.AddControllersWithViews();
+        services.AddRazorPages();
+
+        services.AddDbContextPool<GestioneSagreDbContext>(optionBuilder =>
         {
-            Configuration = configuration;
-        }
+            var maxRetryCount = Configuration.GetSection("Database").GetValue<int>("maxRetryCount");
+            var maxRetryDelay = TimeSpan.FromSeconds(Configuration.GetSection("Database").GetValue<double>("maxRetryDelay"));
+            var connectionString = Configuration.GetSection("ConnectionStrings").GetValue<string>("Default");
 
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddControllersWithViews();
-            services.AddRazorPages();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+            optionBuilder.UseSqlServer(connectionString, options =>
             {
-                app.UseDeveloperExceptionPage();
-                app.UseWebAssemblyDebugging();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
+                // To perform a new migration you need:
 
-            app.UseHttpsRedirection();
-            app.UseBlazorFrameworkFiles();
+                // 1. Open the Package Manager Console panel
 
-            app.UseStaticFiles();
-            app.UseRouting();
+                // 2. In the Default Project drop-down menu make sure that the selected project is GestioneSagre.Web.Server.
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapControllers();
-                endpoints.MapFallbackToFile("index.html");
+                // 3. Finally run the command Add-Migration NAME-MIGRATION -Project GestioneSagre.Web.Migrations
+                // where NAME-MIGRATION represents the name of the migration to create (example: InitialMigration)
+
+                // Info su: https://docs.microsoft.com/it-it/ef/core/managing-schemas/migrations/projects?tabs=dotnet-core-cli
+                options.MigrationsAssembly("GestioneSagre.Web.Migrations");
+                options.EnableRetryOnFailure(maxRetryCount, maxRetryDelay, null);
             });
+        });
+
+        // Services - Custom Extension Method
+        services.AddApplicationServices(Configuration);
+        services.AddSwaggerServices(Configuration);
+
+        // Options
+        services.Configure<KestrelServerOptions>(Configuration.GetSection("Kestrel"));
+    }
+
+    public void Configure(WebApplication app)
+    {
+        IWebHostEnvironment env = app.Environment;
+
+        if (env.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
         }
+
+        var enableSwagger = Configuration.GetSection("Swagger").GetValue<bool>("enabled");
+
+        if (enableSwagger)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gestione Sagre v1"));
+        }
+
+        app.UseApplicationServices(); // Custom Extension Method
+        app.UseBlazorFrameworkFiles();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapFallbackToFile("index.html");
+        });
     }
 }
