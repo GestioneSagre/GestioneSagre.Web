7ccea8460d4cdfc2434f2beb6fafd7bd455b7d1f
Angelo Pirola <angelo9871@gmail.com>
2022-07-16 01:54:34 +0200

    Refactoring GestioneSagre.Web.Client

---
+++ src/GestioneSagre.Web.Client/App.razor.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
+
+namespace GestioneSagre.Web.Client;
+
+public class AppBase : ComponentBase, IDisposable
+{
+    [Inject] private LazyAssemblyLoader AssemblyLoader { get; set; }
+    [Inject] private ILogger<App> Logger { get; set; }
+
+    protected readonly List<Assembly> LazyLoadedAssemblies = new();
+
+    protected async Task OnNavigateAsync(NavigationContext args)
+    {
+        try
+        {
+            switch (args.Path)
+            {
+                //case "fetchdata":
+                //    {
+                //        var assemblies = await AssemblyLoader.LoadAssembliesAsync(new List<string>
+                //        {
+                //            "GestioneSagre.Web.Client.WeatherForecast.dll"
+                //        });
+                //        LazyLoadedAssemblies.AddRange(assemblies);
+                //        break;
+                //    }
+
+                default:
+                    {
+                        var assemblies = await AssemblyLoader.LoadAssembliesAsync(new List<string>
+                        {
+                            "GestioneSagre.Web.UI.dll"
+                        });
+
+                        LazyLoadedAssemblies.AddRange(assemblies);
+                        break;
+                    }
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError($"Error Loading spares page: {ex}");
+        }
+    }
+
+    public void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+        }
+    }
+
+    public void Dispose()
+    {
+        this.Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    ~AppBase()
+    {
+        this.Dispose(false);
+    }
+}
--- src/GestioneSagre.Web.Client/Program.cs
+++ src/GestioneSagre.Web.Client/Program.cs
@@ -1,12 +1,8 @@
+using GestioneSagre.Web.Services.Services.Versioni;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+using MudBlazor;
+using MudBlazor.Services;
 
 namespace GestioneSagre.Web.Client
 {
@@ -16,8 +12,27 @@ namespace GestioneSagre.Web.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            // Services SCOPED
+            builder.Services.Scan(scan => scan.FromAssemblyOf<VersioneService>()
+                 .AddClasses(classes => classes.Where(type => type.Name.EndsWith("Service")))
+                 .AsImplementedInterfaces()
+                 .WithScopedLifetime());
+
+            builder.Services.AddMudServices(config =>
+            {
+                config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter;
+                config.SnackbarConfiguration.PreventDuplicates = false;
+                config.SnackbarConfiguration.NewestOnTop = false;
+                config.SnackbarConfiguration.ShowCloseIcon = false;
+                config.SnackbarConfiguration.VisibleStateDuration = 3000; //5000;
+                config.SnackbarConfiguration.HideTransitionDuration = 500;
+                config.SnackbarConfiguration.ShowTransitionDuration = 500;
+                config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+            });
 
             await builder.Build().RunAsync();
         }
