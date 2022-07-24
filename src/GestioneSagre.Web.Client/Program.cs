d87880259052e62715f666ee62e843802c0a87d9
Angelo Pirola <angelo9871@gmail.com>
2022-07-24 19:10:53 +0200

    Refactoring

--- src/GestioneSagre.Web.Client/Program.cs
+++ src/GestioneSagre.Web.Client/Program.cs
@@ -15,7 +15,10 @@ namespace GestioneSagre.Web.Client
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
 
             //Services SCOPED
             builder.Services.Scan(scan => scan.FromAssemblyOf<VersioneService>()
